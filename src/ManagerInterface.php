45e922991aa3e88b581d5834635634bc56ad0f0e
Graham Campbell <graham@mineuk.com>
2015-03-15 22:14:17 +0000

    Add a manager interface

---
+++ src/ManagerInterface.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of NotifyMe.
+ *
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NotifyMeHQ\NotifyMe;
+
+interface ManagerInterface
+{
+    /**
+     * Get a connection instance.
+     *
+     * @param string|null $name
+     *
+     * @return \NotifyMeHQ\NotifyMe\GatewayInterface
+     */
+    public function connection($name = null);
+
+    /**
+     * Reconnect to the given connection.
+     *
+     * @param string|null $name
+     *
+     * @return \NotifyMeHQ\NotifyMe\GatewayInterface
+     */
+    public function reconnect($name = null);
+
+    /**
+     * Disconnect from the given connection.
+     *
+     * @param string|null $name
+     *
+     * @return void
+     */
+    public function disconnect($name = null);
+
+    /**
+     * Get the configuration for a connection.
+     *
+     * @param string $name
+     *
+     * @return array
+     */
+    public function getConnectionConfig($name);
+
+    /**
+     * Get the default connection name.
+     *
+     * @return string
+     */
+    public function getDefaultConnection();
+
+    /**
+     * Set the default connection name.
+     *
+     * @param string $name
+     *
+     * @return void
+     */
+    public function setDefaultConnection($name);
+
+    /**
+     * Return all of the created connections.
+     *
+     * @return \NotifyMeHQ\NotifyMe\GatewayInterface[]
+     */
+    public function getConnections();
+}
